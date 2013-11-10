0dc018fad16a5d4b27790846ac3d404374270615
Jan Peca <theboxercz@gmail.com>
2013-11-10 16:07:33 +0100

    Build script updated

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -11,13 +11,14 @@
  */
 $snippets = array();
 
-//$snippets[0]= $modx->newObject('modSnippet');
-//$snippets[0]->fromArray(array(
-//    'id' => 0,
-//    'name' => 'UrlShortener',
-//    'description' => 'Displays Items.',
-//    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.urlshortener.php'),
-//),'',true,true);
+$snippets[0]= $modx->newObject('modSnippet');
+$snippets[0]->fromArray(array(
+    'id' => 0,
+    'name' => 'UrlShortener',
+    'description' => 'Shorten given URL',
+    'snippet' => getSnippetContent($sources['snippets'] . 'urlshortener.snippet.php'),
+),'',true,true);
+
 //$properties = include $sources['build'].'properties/properties.urlshortener.php';
 //$snippets[0]->setProperties($properties);
 //unset($properties);
